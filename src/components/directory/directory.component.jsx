17cccf508bfce2b718d4e03dfc6b2e0ee239c4fe
Alexander <Snakeage@gmail.com>
2020-04-07 19:46:09 +0300

    added checkout page, checkout item component and updated to use selectors from reselect

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -20,27 +20,27 @@ class Directory extends React.Component {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
-          linkUrl: 'jackets'
+          linkUrl: ''
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
-          linkUrl: 'sneakers'
+          linkUrl: ''
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
-          linkUrl: 'womens'
+          linkUrl: ''
         },
         {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
-          linkUrl: 'mens'
+          linkUrl: ''
         }
       ]
     };
