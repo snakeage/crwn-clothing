cd69b4ff9036a0a756047cf632ff740df4db2e9e
Alexander <Snakeage@gmail.com>
2020-04-07 11:44:29 +0300

    created cart-item component and connected our cart dropdown with cartItems reducer

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -23,12 +23,11 @@ class SignIn extends React.Component {
     const { email, password } = this.state;
 
     try {
-        await auth.signInWithEmailAndPassword(email, password);
-        this.setState({ email: '', password: '' });
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-
   };
 
   handleChange = event => {
