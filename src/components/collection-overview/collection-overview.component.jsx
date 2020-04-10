23d9457b31afa1243d0603496a9b2e26e5025b3a
Alexander <Snakeage@gmail.com>
2020-04-10 18:35:51 +0300

    implement code that puls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included our WithSpinner HOC

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -6,14 +6,14 @@ import CollectionPreview from '../collection-preview/collection-preview.componen
 
 import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
-import './collection-overview.styles.scss';
+import { CollectionsOverviewContainer } from './collection-overview.styles';
 
 const CollectionsOverView = ({ collections }) => (
-    <div className='collections-overview'>
+    <CollectionsOverviewContainer>
         {collections.map(({ id, ...otherCollectionProps }) => (
 		    <CollectionPreview key={id} {...otherCollectionProps} />
 		))}
-    </div>
+    </CollectionsOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,70 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CollectionsOverView from '../../components/collection-overview/collection-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match }) => (
-	<div className='shop-page'>
-		<Route exact path={`${match.path}`} component={CollectionsOverView} />
-		<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-	</div>
-);
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-export default ShopPage;
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+import { updateCollections } from '../../redux/shop/shop.actions';
+
+const CollectionsOverViewWithSpinner = WithSpinner(CollectionsOverView);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends React.Component {
+	state = {
+		loading: true
+	};
+
+	unsubscribeFromSnapshot = null;
+
+	componentDidMount() {
+		const { updateCollections } = this.props;
+		const collectionRef = firestore.collection('collections');
+
+		fetch('https://firestore.googleapis.com/v1/projects/crwn-db-cab71/databases/(default)/documents/collections'
+		)
+		.then(response => response.json())
+		.then(collections => console.log(collections))
+
+		// collectionRef.get().then(snapshot => {
+		// 	const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+		// 	updateCollections(collectionsMap);
+		// 	this.setState({ loading: false });
+		// });
+	}
+
+	render() {
+		const { match } = this.props;
+		const { loading } = this.state;
+		return (
+			<div className='shop-page'>
+				<Route exact 
+					path={`${match.path}`} 
+					render={props => (
+						<CollectionsOverViewWithSpinner isLoading={loading} {...props} />
+					)}
+				/>
+				<Route 
+					path={`${match.path}/:collectionId`} 
+					render={props => (
+						<CollectionPageWithSpinner isLoading={loading} {...props} />
+					)} 
+				/>
+			</div>
+		);
+	}
+}
+
+const mapDispatchToProps = dispatch => ({
+	updateCollections: collectionsMap => 
+	dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(
+	null, 
+	mapDispatchToProps
+	)(ShopPage);
