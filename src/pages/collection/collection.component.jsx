18b121c512d3035b3c7a2146c83d4899688f3eb8
Alexander <Snakeage@gmail.com>
2020-04-12 18:51:43 +0300

    updated our application App component, sign-in component and sign-up componentto use hooks

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import { firestore } from '../../firebase/firebase.utils';
 
 import {
   CollectionPageContainer,
@@ -12,6 +14,16 @@ import {
 } from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
+  useEffect(() => {
+    console.log('I am subscribing');
+    const unsubscribeFromCollections = firestore.collection('collections').onSnapshot(snapshot => console.log(snapshot))
+
+    return () => {
+      console.log('I am unsubscribing');
+      unsubscribeFromCollections();
+    };
+  }, []);
+
   const { title, items } = collection;
   return (
     <CollectionPageContainer>
